a40f0e919b213bab7e69e2da4e002da7713f44b5
jisooHong <ghdwltn0204@naver.com>
2021-11-02 02:35:34 +0900

    add group db

---
+++ backend/src/main/java/com/palette/domain/group/Expense.java
@@ -0,0 +1,30 @@
+package com.palette.domain.group;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //종류 : 어떤 종류의 지출인지(교통,식비,숙박,기타)
+    private String category;
+
+    //자세한 내용 : 지출의 자세한 내용
+    private String detail;
+
+    //가격
+    private Long price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "budget_id")
+    private Budget budget;
+
+}
