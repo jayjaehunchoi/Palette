d2dbc5f4a664807fb27c19654d552ac403f8159a
HGI <ghdruddls999@naver.com>
2021-11-03 00:23:13 +0900

    추가 : google 로그인 연동

--- backend/src/main/java/com/palette/domain/member/Member.java
+++ backend/src/main/java/com/palette/domain/member/Member.java
@@ -19,10 +19,20 @@ public class Member extends BaseTimeEntity {
     private String upw;
     private String uname;
     private String profileFileName;
+    private String email;
 
-    public Member(String upw, String uname, String profileFileName) {
+    @Builder
+    public Member(String upw, String uname, String profileFileName, String email) {
         this.upw = upw;
         this.uname = uname;
         this.profileFileName = profileFileName;
+        this.email = email;
+    }
+
+    public Member update(String uname, String profileFileName) {
+        this.uname = uname;
+        this.profileFileName = profileFileName;
+
+        return this;
     }
 }
--- backend/src/main/java/com/palette/repository/MemberRepository.java
+++ backend/src/main/java/com/palette/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.palette.repository;
 import com.palette.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String email); //소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 판단
 }
