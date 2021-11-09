b5b19120fdd3a515eeee489254fbbc4ebc30f4ca
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-09 17:34:17 +0900

    추가 : XSS 보안을 위한 Config 추가

--- backend/src/main/java/com/palette/config/WebConfig.java
+++ backend/src/main/java/com/palette/config/WebConfig.java
@@ -1,17 +1,31 @@
 package com.palette.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.palette.utils.HtmlCharacterEscapes;
 import com.palette.utils.argumentresolver.LoginArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final ObjectMapper objectMapper;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginArgumentResolver());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
     }
 }
