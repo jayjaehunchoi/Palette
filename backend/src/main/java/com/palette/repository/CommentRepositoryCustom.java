1a7b40bcab4ca386c7004bb6fabf9d429b7c64a0
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-18 01:47:30 +0900

    리팩토링 : Repository 애노테이션 제거

--- backend/src/main/java/com/palette/repository/CommentRepositoryCustom.java
+++ backend/src/main/java/com/palette/repository/CommentRepositoryCustom.java
@@ -1,8 +1,7 @@
 package com.palette.repository;
 
-import com.palette.domain.post.Comment;
 import com.palette.dto.response.CommentResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
